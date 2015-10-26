0dc200182092ee93da6635cc326627a0a0bf8c7e
Damien Maillard <dmaillard06@gmail.com>
2015-10-26 11:50:09 +0100

    rename store service

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-import ressource from './lib/ressource.js';
-import stores from './lib/store/store-#{platform-type}.js';
+import rest from './lib/rest.js';
 
-for(var storeName in stores){
-	ressource.define(storeName, stores[storeName]);
-}
-
-var supportedRessources = ressource.stores.map(function(store){
-	return store.name + '=' + Object.keys(store.properties.methods).join(' ');
-});
-
-console.log('supported ressources', supportedRessources.join(', '));
-
-export default ressource;
+export default rest;
